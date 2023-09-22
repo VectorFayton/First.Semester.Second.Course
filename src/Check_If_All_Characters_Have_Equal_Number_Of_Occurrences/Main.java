54f7caf54366087d38cc893443d8139c2391e912
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-09-22 21:36:22 +0600

    Check If All Characters Have_Equal Number Of Occurrences

--- src/Check_If_All_Characters_Have_Equal_Number_Of_Occurrences/Main.java
+++ src/Check_If_All_Characters_Have_Equal_Number_Of_Occurrences/Main.java
@@ -1,8 +1,6 @@
 package Check_If_All_Characters_Have_Equal_Number_Of_Occurrences;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,23 +10,43 @@ public class Main {
 
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        HashSet<String> hash_string = new HashSet<>();
-        HashMap<Integer, String> hash_map_string = new HashMap<>();
-        boolean are_occurrence = false;
-        int counter = 0;
+//        HashSet<String> hash_string = new HashSet<>();
+//        TreeMap<String, Integer, String> hash_map_string = new HashMap<>();
+//        boolean are_occurrence = false;
+//        int counter = 0;
+//        char[] string_by_character = s.toCharArray();
+//        for (int i = 0; i < string_by_character.length; i++) {
+//            String character = String.valueOf(string_by_character[i]);
+//            hash_string.add(String.valueOf(string_by_character[i]));
+//            hash_string.add(character);
+//            if (!hash_map_string.containsValue(character)){
+//                hash_map_string.put(character, i);
+//            } else if(hash_map_string.containsValue(character)) {
+//                hash_map_string.put(character, 0);
+//            }
+//        }
+//        Set<String> keys = hash_map_string.keySet();
+//        List<Integer> values = new ArrayList<>(hash_map_string.values());
+//        System.out.println(hash_string);
+//        System.out.println(keys);
+//        System.out.println(values);
+//        System.out.println(hash_map_string);
+//        System.out.println(s.length());
+//        System.out.println(counter);
+//        System.out.println(hash_string.size());
+//        return false;
         char[] string_by_character = s.toCharArray();
+        Map<String, Integer> string_map = new HashMap<>();
+
+
         for (int i = 0; i < string_by_character.length; i++) {
             String character = String.valueOf(string_by_character[i]);
-            hash_string.add(String.valueOf(string_by_character[i]));
-            if (!hash_map_string.containsValue(character)){
-                hash_map_string.put(1, character);
-            } else {
-//                hash_map_string();
-            }
+            string_map.put(character, string_map.getOrDefault(character, 0) + 1);
         }
-        System.out.println(s.length());
-        System.out.println(counter);
-        System.out.println(hash_string.size());
-        return false;
+        System.out.println(string_map);
+        Set<Integer> unique_frequency = new HashSet<>(string_map.values());
+        System.out.println(unique_frequency);
+
+        return unique_frequency.size() == 1;
     }
 }
