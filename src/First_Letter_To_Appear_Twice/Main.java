8e3daf7dcd6c07be9b02a8383dd3882a02862e24
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-09-22 18:31:45 +0600

    First Letter to Appear Twice

---
+++ src/First_Letter_To_Appear_Twice/Main.java
@@ -0,0 +1,14 @@
+package First_Letter_To_Appear_Twice;
+
+class Solution {
+    public char repeatedCharacter(String s) {
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().repeatedCharacter("abccbaacz"));
+    }
+}
+
