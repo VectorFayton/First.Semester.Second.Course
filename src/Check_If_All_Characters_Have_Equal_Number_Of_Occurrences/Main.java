7640715629ca5718cb308d05e72c3fd927701723
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-09-22 20:32:23 +0600

    Check If All Characters Have_Equal Number Of Occurrences

--- src/Check_If_All_Characters_Have_Equal_Number_Of_Occurrences/Main.java
+++ src/Check_If_All_Characters_Have_Equal_Number_Of_Occurrences/Main.java
@@ -1,4 +1,34 @@
 package Check_If_All_Characters_Have_Equal_Number_Of_Occurrences;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Objects;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().areOccurrencesEqual("havkaoxlnwuupdwkzeuwshymficktljworbqgdunhwxmkveakywgohayjjrhqtvt"));
+    }
+}
+
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        HashSet<String> hash_string = new HashSet<>();
+        HashMap<Integer, String> hash_map_string = new HashMap<>();
+        boolean are_occurrence = false;
+        int counter = 0;
+        char[] string_by_character = s.toCharArray();
+        for (int i = 0; i < string_by_character.length; i++) {
+            String character = String.valueOf(string_by_character[i]);
+            hash_string.add(String.valueOf(string_by_character[i]));
+            if (!hash_map_string.containsValue(character)){
+                hash_map_string.put(1, character);
+            } else {
+//                hash_map_string();
+            }
+        }
+        System.out.println(s.length());
+        System.out.println(counter);
+        System.out.println(hash_string.size());
+        return false;
+    }
 }
--- src/First_Letter_To_Appear_Twice/Main.java
+++ src/First_Letter_To_Appear_Twice/Main.java
@@ -1,22 +1,26 @@
 package First_Letter_To_Appear_Twice;
 // https://leetcode.com/problems/first-letter-to-appear-twice/solutions/4076705/220103036/
+// https://leetcode.com/problems/first-letter-to-appear-twice/solutions/4076985/220103036-hash-map/
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 class Solution {
     public char repeatedCharacter(String s) {
-        HashSet<String> hash_string = new HashSet<>();
-        char[] string_by_alphabet = s.toCharArray();
-        char charming = s.charAt(0);
-        for(int i = 0; i < s.length(); i++){
-            hash_string.add(String.valueOf(string_by_alphabet[i]));
-//            System.out.printf("HashSize: %s, Index: %s\n", hash_string.size(), (i + 1));
-            if(hash_string.size() != (i + 1)){
-                charming = String.valueOf(string_by_alphabet[i]).charAt(0);
+        HashMap<Integer, String> hash_map_string = new HashMap<>();
+        char[] string_by_character = s.toCharArray();
+        char answer = s.charAt(0);
+        for (int i = 0; i < string_by_character.length; i++) {
+            String character = String.valueOf(string_by_character[i]);
+            if (!hash_map_string.containsValue(character)){
+                hash_map_string.put(i, character);
+            } else {
+                hash_map_string.put(i, character);
+                answer = character.charAt(0);
                 break;
             }
         }
-        return charming;
+        return answer;
     }
 }
 
