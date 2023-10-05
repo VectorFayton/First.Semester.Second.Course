bc34b5418ae2e0e3f3978db63a873c911900f2a5
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-10-05 21:23:32 +0600

    Ally

--- src/Priority_Queue/Main.java
+++ src/Priority_Queue/Main.java
@@ -1,4 +1,18 @@
 package Priority_Queue;
 
 public class Main {
+    public static void main(String[] args) {
+        EventPlanner events = new EventPlanner();
+        events.addEvent("Birthday of nothing", 1);
+        events.addEvent("Birthday of Mom", 2);
+        events.addEvent("Birthday of Sister", 3);
+        events.addEvent("Birthday of Son", 4);
+        events.addEvent("Birthday of Dad", 5);
+        events.addEvent("Birthday of Friend", 6);
+        events.addEvent("Birthday of Wife", 7);
+        events.addEvent("New year", 8);
+        events.addEvent("Party", 9);
+        System.out.println(events.isEmpty());
+        System.out.println(events.getNextEvent());
+    }
 }
